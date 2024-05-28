c238b11c48d962a3e3046019304435494e6d878c
Sergio Ferreira <dev.sergioferreira@gmail.com>
2024-05-28 07:56:48 -0300

    att de vetores em C

--- testes/Vetor.c
+++ testes/Vetor.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
 
-    int x , y;
+    int x, y;
 
     printf("Digite quantos numeros deseja guardar: ");
     scanf("%i", &x);
 
     double vet[x];
 
-    for (y = 0; y < x; y++){
+    for (y = 0; y < x; y++)
+    {
         printf("Digite os numeros que serao salvos no vetor: ");
         scanf("%lf", &vet[y]);
     }
     printf("\n Os numeros digitados foram: \n");
-    for(y = 0 ; y < x ; y++){
+    for(y = 0 ; y < x ; y++)
+    {
         printf("%.2lf \n", vet[y]);
     }
     return 0;
+
+
 }
