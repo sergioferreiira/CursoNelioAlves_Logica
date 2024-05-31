933ab6718a87fa6b53c36174471ccce0fd529781
Sergio Ferreira <dev.sergioferreira@gmail.com>
2024-05-31 08:20:28 -0300

    Aula de Matriz

---
+++ aulas/matrizesEmC.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+int main() {
+
+    int L , C , NL , NC;
+
+
+    printf("Quantas linhas vai ter sua matriz? ");
+    scanf("%d", &L);
+    printf("Quantas colunas vai ter sua matriz? ");
+    scanf("%d", &C);
+
+    int mat[L][C];
+
+    for(NL = 0; NL < L; NL++){
+        for(NC = 0; NC < C; NC++){
+            printf("Digite o numero a ser salvo na matriz[%d , %d] :", NL, NC);
+            scanf("%d", &mat[NL][NC]);
+        }
+    }
+
+    for(NL = 0; NL < L; NL++){
+        for(NC = 0; NC < C; NC++){
+            printf("%d ", mat[NL][NC]);
+        }
+        printf("\n");
+    }
+}
