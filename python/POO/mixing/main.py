eee4da4b2210363b14a16223af4f82220960e1db
Sergio Ferreira <dev.sergioferreira@gmail.com>
2024-06-17 20:05:26 -0300

    Atts

--- python/POO/mixing/main.py
+++
@@ -1,6 +0,0 @@
-from log import LogFileMixin , LogPrintMixin
-
-
-
-lteste = LogFileMixin()
-lteste.log_sucess('Importei e testei olha que legal')
--- python/exercises/loading_json.py
+++
@@ -1,6 +0,0 @@
-import json
-
-
-with open('salvando_exercicio.json', 'r' , encoding='utf-8') as arquivo:
-    mouse = json.load(arquivo)
-    print(mouse)
