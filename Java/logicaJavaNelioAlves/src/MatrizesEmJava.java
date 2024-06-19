2aa5084b93d069719d1f72c083e89c8b7fee2f30
Sergio Ferreira <dev.sergioferreira@gmail.com>
2024-06-19 07:59:24 -0300

    More exercises with java

--- Java/logicaJavaNelioAlves/src/MatrizesEmJava.java
+++ Java/logicaJavaNelioAlves/src/MatrizesEmJava.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 
 public class MatrizesEmJava {
 	
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
@@ -32,7 +35,8 @@ public class MatrizesEmJava {
 //		}
 		
 		
-		
+		 
+//		Problema "diagonal_negativos" 
 //		int m;
 //		int negatives = 0;
 //		
@@ -70,42 +74,96 @@ public class MatrizesEmJava {
 //		
 		
 		
-		int m, n;
-		double sum1 = 0, sum2 = 0;
 		
-		System.out.println("how many lines will be there? ");
+
+//		Problema "soma_linhas"  
+//		int m, n;
+//		double sum1 = 0, sum2 = 0;
+//		
+//		System.out.println("how many lines will be there? ");
+//		m = sc.nextInt();
+//		System.out.println("how many colluns will be there?");
+//		n = sc.nextInt();
+//		
+//		double[][] mat = new double[m][n];
+//		
+//		for (int i = 0; i < m; i++) {
+//			System.out.println("Type the elements from the " + i + "a. line:");
+//			for (int j = 0; j < n; j++) {
+//				mat[i][j] = sc.nextDouble();
+//			}
+//		}
+//		
+//		for (int i = 0; i == 0;i++) {
+//			for (int j = 0; j < n; j++) {
+//				sum1 = sum1 + mat[i][j];
+//			}
+//		}
+//		for (int i = 1; i < m;i++) {
+//			for (int j = 0; j < n; j++) {
+//				sum2 = sum2 + mat[i][j];
+//			}
+//		}
+//		
+//		System.out.println("Generated vector:");
+//		System.out.println(String.format("%.1f", sum1));
+//		System.out.println(String.format("%.1f", sum2));
+//		
+		
+		
+		
+//		int m, n;
+//		
+//		System.out.println("how many lines will be there? ");
+//		m = sc.nextInt();
+//		System.out.println("how many colluns will be there?");
+//		n = sc.nextInt();
+//		
+//		int[][] mat = new int[m][n];
+//		
+//		for (int i = 0; i < m; i++) {
+//			for (int j = 0; j < n; j++) {
+//				System.out.println("Element" + "[" + i + "]" + "," + "[" + j + "]" + " :" );
+//				mat[i][j] = sc.nextInt();
+//			}
+//		}
+//		
+//		for (int i = 0; i < m; i++) {
+//			for (int j = 0; j < n; j++) {
+//				if (mat[i][j] < 0) {
+//				System.out.print(mat[i][j] + " ");
+//				}
+//			}
+//		}
+		
+		int m ;
+		int x =0;
+		
+		System.out.println("how many lines and collums will be there? ");
 		m = sc.nextInt();
-		System.out.println("how many colluns will be there?");
-		n = sc.nextInt();
-		
-		double[][] mat = new double[m][n];
+
+		int[][] mat = new int[m][m];
 		
 		for (int i = 0; i < m; i++) {
-			System.out.println("Type the elements from the " + i + "a. line:");
-			for (int j = 0; j < n; j++) {
-				mat[i][j] = sc.nextDouble();
+			for (int j = 0; j < m; j++) {
+				System.out.println("Element" + "[" + i + "]" + "," + "[" + j + "]" + " :" );
+				mat[i][j] = sc.nextInt();
 			}
+			
 		}
 		
-		for (int i = 0; i == 0;i++) {
-			for (int j = 0; j < n; j++) {
-				sum1 = sum1 + mat[i][j];
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < m; j++) 
+			{
+				if(mat[i][j] > x) {
+					x = mat[i][j];
+				}
+				
 			}
+			System.out.println("Bigest element in each line: ");
+			System.out.println(x);
+			x = 0;
 		}
-		for (int i = 1; i < m;i++) {
-			for (int j = 0; j < n; j++) {
-				sum2 = sum2 + mat[i][j];
-			}
-		}
-		
-		System.out.println("Generated vector:");
-		System.out.println(String.format("%.1f", sum1));
-		System.out.println(String.format("%.1f", sum2));
-		
-		
-		
-		
-		
 		
 		
 		
