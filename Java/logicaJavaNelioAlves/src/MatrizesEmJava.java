fa30a45cbeed866b8300b54530b4aac4d42ea00d
Sergio Ferreira <dev.sergioferreira@gmail.com>
2024-06-19 22:47:42 -0300

    More exercises in java

--- Java/logicaJavaNelioAlves/src/MatrizesEmJava.java
+++ Java/logicaJavaNelioAlves/src/MatrizesEmJava.java
@@ -136,42 +136,94 @@ public class MatrizesEmJava {
 //			}
 //		}
 		
-		int m ;
-		int x =0;
+//		int m ;
+//		int x =0;
+//		
+//		System.out.println("how many lines and collums will be there? ");
+//		m = sc.nextInt();
+//
+//		int[][] mat = new int[m][m];
+//		
+//		for (int i = 0; i < m; i++) {
+//			for (int j = 0; j < m; j++) {
+//				System.out.println("Element" + "[" + i + "]" + "," + "[" + j + "]" + " :" );
+//				mat[i][j] = sc.nextInt();
+//			}
+//			
+//		}
+//		
+//		for (int i = 0; i < m; i++) {
+//			for (int j = 0; j < m; j++) 
+//			{
+//				if(mat[i][j] > x) {
+//					x = mat[i][j];
+//				}
+//				
+//			}
+//			System.out.println("Bigest element in each line: ");
+//			System.out.println(x);
+//			x = 0;
+//		}
 		
-		System.out.println("how many lines and collums will be there? ");
+		
+//		int m, n;
+//		
+//		System.out.println("how many lines will be there? ");
+//		m = sc.nextInt();
+//		System.out.println("how many colluns will be there?");
+//		n = sc.nextInt();
+//		
+//		int[][] mat = new int[m][n];
+//		int[][] matb = new int[m][n];
+//		
+//		for (int i = 0; i < m; i++) {
+//			for (int j = 0; j < n; j++) {
+//				System.out.println("[" + i + "]" + "," + "[" + j + "]" + " :" );
+//				mat[i][j] = sc.nextInt();
+//			}
+//		}
+//		for (int i = 0; i < m; i++) {
+//			for (int j = 0; j < n; j++) {
+//				System.out.println("[" + i + "]" + "," + "[" + j + "]" + " :" );
+//				matb[i][j] = sc.nextInt();
+//			}
+//		}
+//		
+//		System.out.println();
+//		for (int i = 0; i < m; i++) {
+//			for (int j = 0; j < n; j++) {
+//				mat[i][j] = mat[i][j] + matb[i][j];
+//				System.out.print(mat[i][j] + " ");
+//			}
+//			System.out.println(" ");
+//		}
+		
+		
+		
+		int m,n = 1, sum = 0;
+		
+		System.out.println("how many lines will be there? ");
 		m = sc.nextInt();
 
+		
 		int[][] mat = new int[m][m];
 		
 		for (int i = 0; i < m; i++) {
 			for (int j = 0; j < m; j++) {
-				System.out.println("Element" + "[" + i + "]" + "," + "[" + j + "]" + " :" );
+				System.out.print("[" + i + "]" + "," + "[" + j + "]" + " :" );
 				mat[i][j] = sc.nextInt();
 			}
-			
 		}
 		
 		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < m; j++) 
-			{
-				if(mat[i][j] > x) {
-					x = mat[i][j];
-				}
-				
+			for (int j = n; j < m; j++) {
+				sum = sum + mat[i][j];
 			}
-			System.out.println("Bigest element in each line: ");
-			System.out.println(x);
-			x = 0;
+			n++;
 		}
 		
 		
-		
-		
-		
-		
-		
-		
+		System.out.println("The sum of the elements above the main diagonal is: " + sum);
 		
 		
 		
